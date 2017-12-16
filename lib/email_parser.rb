9ea8bb09cfb474384cdd9f92bc658a1e11ef6877
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2017-12-16 16:00:52 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -5,6 +5,8 @@
 
 class EmailParser
   attr_accessor :emails
+
+  def initialize(emails)
   @emails = emails
 end
 
@@ -12,4 +14,5 @@ def parse
   emails.split.map do |email|
     email.split(',')
   end.flatten.uniq
+end
 end
